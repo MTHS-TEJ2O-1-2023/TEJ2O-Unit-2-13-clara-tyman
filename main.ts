6e4300be6c7d93d0301ae0544ba203f9be89275a
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-02 12:46:41 -0400

    linter fix

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Clara
+ * Created on: Oct 2023
+ * This program counts down from 4 to 0 and lights up neopixels
 */
 
-basic.showString('Hello, World!')
+let loopCounter: number = 4
+let neopixelStrip: neopixel.Strip = null
+
+basic.showIcon(IconNames.Silly)
+basic.clearScreen()
+basic.pause(500)
+
+// cleanup
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+// press "A" button
+input.onButtonPressed(Button.A, function () {
+
+  // setup
+  basic.clearScreen()
+  let loopCounter = 4
+
+    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
+
+  // loop
+  while (loopCounter >= 0){
+    basic.pause(500)
+    basic.showNumber(loopCounter)
+    neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    loopCounter = loopCounter - 1
+  }
+})
